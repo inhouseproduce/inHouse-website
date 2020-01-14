ad4e19499200cce0260f7e9208b959d5e74bb9aa
Donovan Lopez <donovanl2468@gmail.com>
2020-01-14 13:23:47 -0800

    text messages part 7

--- send_sms.js
+++ send_sms.js
@@ -7,6 +7,8 @@ var cronJobsArray = [];
 const textReminders = require('./text-reminders.json');
 const configContent = require('./config.json');
 const fetch = require("node-fetch");
+const twilioNo = process.env.TWILIO_PHONE;
+
 
 module.exports = ( app ) => {
 
@@ -47,7 +49,7 @@ function sendMessage(phoneNo, message)
     {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"to":phoneNo,"body": message})
+    body: JSON.stringify({"from":twilioNo, "to":phoneNo,"body": message})
   }).catch(err => console.error("Here's an error: " + err));
 }
 
