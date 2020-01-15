9f37dbb9311292936cba9a0eececdf0027cd1eed
Donovan Lopez <donovanl2468@gmail.com>
2020-01-15 13:50:58 -0800

    text messages part 9

--- server-config/mongodb.js
+++ server-config/mongodb.js
@@ -1,28 +1,38 @@
 // const mongoose = require('mongoose');
-const mongodb = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 module.exports = () => {
     const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/inhouse';
 
-    const mdbConfig = {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        promiseLibrary: global.Promise
-    };
+    // const mdbConfig = {
+    //     useCreateIndex: true,
+    //     useNewUrlParser: true,
+    //     useFindAndModify: false,
+    //     useUnifiedTopology: true,
+    //     promiseLibrary: global.Promise
+    // };
 
-    mongodb.connect(MONGODB_URI, function(err, db){
-        console.log('mongodb connection successful');
+    const client = new MongoClient(MONGODB_URI, {useNewUrlParser: true});
 
-        const collection = db.collection('clients');
-
-        // Find some documents
-        collection.find({}).toArray(function(err, docs) {
-          console.log("Found the following records");
-          console.log(docs)
-        });
+    // Use connect method to connect to the server
+    client.connect(function(err) {
+      console.log("Connected successfully to server");
+    
+      const db = client.db(dbName);
+    
     });
+
+    // mongodb.connect(MONGODB_URI, function(err, db){
+    //     console.log('mongodb connection successful');
+
+    //     const collection = db.collection('clients');
+
+    //     // Find some documents
+    //     collection.find({}).toArray(function(err, docs) {
+    //       console.log("Found the following records");
+    //       console.log(docs)
+    //     });
+    // });
     // mongoose.connect(MONGODB_URI, mdbConfig);
     // let mongoose_connection = mongoose.connection
 
