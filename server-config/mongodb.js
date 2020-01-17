5c5536116c9e69c52e203bc95caf2d74893f240d
Donovan Lopez <donovanl2468@gmail.com>
2020-01-16 16:51:00 -0800

    text messages part 10

--- server-config/mongodb.js
+++ server-config/mongodb.js
@@ -15,8 +15,38 @@ module.exports = () => {
     mongoose.connect(MONGODB_URI, mdbConfig);
     let mongoose_connection = mongoose.connection
 
+    // mongoose_connection.once('open', () => {
+    //     console.log("connected to mongodb");
+    // });
+
+
+
+
+
+    const Schema = mongoose.Schema;
+
+    const MessagingSchema = new Schema({
+       
+    });
+    
+    const Messenger = mongoose.model("messaging", MessagingSchema, "messaging");
+
+
+
     mongoose_connection.once('open', () => {
-        console.log("connected to mongodb");
+        console.log("here in mongoose connection!");
+        Messenger.find({}, function (err, client_list) 
+        {
+   
+                const client_data = JSON.parse(JSON.stringify(client_list[0]));
+                client_data.clients.forEach(person => 
+                    {
+                    console.log( person.schedule_daily_checkups);
+                });
+         
+
+          });
+
     });
 
     module.exports = mongoose_connection;
