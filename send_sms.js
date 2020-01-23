c3734484539ae608d6cab674e9e3992bd3a5a6a3
Donovan Lopez <donovanl2468@gmail.com>
2020-01-23 11:48:48 -0800

    part 15 - Heroku still doesn't work but debugging through text messages does - part 4

--- send_sms.js
+++ send_sms.js
@@ -17,7 +17,7 @@ module.exports = ( mongoose_connection ) => {
 
   sendMessage(process.env.TESTPHONE, 'seeding 1');
 
-  messageToCustomer(process.env.TESTPHONE, 'seeding');
+  // messageToCustomer(process.env.TESTPHONE, 'seeding');
 
   //at midnight, recheck the jobs 
   // new CronJob("0 50 * * * *", main.bind(this, mongoose_connection),  null, true, "America/Los_Angeles");
