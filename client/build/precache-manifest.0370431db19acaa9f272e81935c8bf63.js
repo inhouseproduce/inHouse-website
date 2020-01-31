693ef58a6e304e182ae784b154852eace1f33717
Eduard Jacobs <edoter@gmail.com>
2020-01-31 12:27:43 -0800

    doc

---
+++ client/build/precache-manifest.0370431db19acaa9f272e81935c8bf63.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9d88fa72c512df566cb606cc1f0bd0",
+    "url": "/datta-able/react/default/index.html"
+  },
+  {
+    "revision": "46ddbefd8287a4214594",
+    "url": "/datta-able/react/default/static/css/1.c3da6537.chunk.css"
+  },
+  {
+    "revision": "d177feda33ada1dd8f2a",
+    "url": "/datta-able/react/default/static/css/6.2bcc536e.chunk.css"
+  },
+  {
+    "revision": "759974d00def43a0303a",
+    "url": "/datta-able/react/default/static/css/7.dda43723.chunk.css"
+  },
+  {
+    "revision": "3b6efdc35bdc62770eb3",
+    "url": "/datta-able/react/default/static/css/8.6ff41549.chunk.css"
+  },
+  {
+    "revision": "2ffd8c2cfa73f6261b05",
+    "url": "/datta-able/react/default/static/js/0.19e192ab.chunk.js"
+  },
+  {
+    "revision": "46ddbefd8287a4214594",
+    "url": "/datta-able/react/default/static/js/1.e0d6189b.chunk.js"
+  },
+  {
+    "revision": "ba86a61e3a8480326e71",
+    "url": "/datta-able/react/default/static/js/10.88d39b4c.chunk.js"
+  },
+  {
+    "revision": "a7cefd39f6388979284a",
+    "url": "/datta-able/react/default/static/js/4.d7e39492.chunk.js"
+  },
+  {
+    "revision": "8379eef08c21b2275731",
+    "url": "/datta-able/react/default/static/js/5.aca19abe.chunk.js"
+  },
+  {
+    "revision": "2fc2158aa36b8ad882f7d21e6f538d1e",
+    "url": "/datta-able/react/default/static/js/5.aca19abe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d177feda33ada1dd8f2a",
+    "url": "/datta-able/react/default/static/js/6.67320433.chunk.js"
+  },
+  {
+    "revision": "36a3fbc68fde5251a7f4ddfe711971ec",
+    "url": "/datta-able/react/default/static/js/6.67320433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "759974d00def43a0303a",
+    "url": "/datta-able/react/default/static/js/7.12384c61.chunk.js"
+  },
+  {
+    "revision": "3b6efdc35bdc62770eb3",
+    "url": "/datta-able/react/default/static/js/8.6e184ca4.chunk.js"
+  },
+  {
+    "revision": "bfaa97ce78c3e0f9e633",
+    "url": "/datta-able/react/default/static/js/9.b4aa8a0a.chunk.js"
+  },
+  {
+    "revision": "48b964f684101d827ddb",
+    "url": "/datta-able/react/default/static/js/main.512e5cea.chunk.js"
+  },
+  {
+    "revision": "392dd20de7569ba7cc46",
+    "url": "/datta-able/react/default/static/js/runtime-main.a044c65c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/datta-able/react/default/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/datta-able/react/default/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/datta-able/react/default/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/datta-able/react/default/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/datta-able/react/default/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1b589b0f17635bca42131dee15d62b8b",
+    "url": "/datta-able/react/default/static/media/navbar-img-2.1b589b0f.jpg"
+  },
+  {
+    "revision": "569bddd6a056fd0bdd8f5b1c11c12c82",
+    "url": "/datta-able/react/default/static/media/navbar-img-3.569bddd6.jpg"
+  },
+  {
+    "revision": "cc57986b40b4902f27595b5c8a4eb2b9",
+    "url": "/datta-able/react/default/static/media/navbar-img-4.cc57986b.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/datta-able/react/default/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/datta-able/react/default/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/datta-able/react/default/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/datta-able/react/default/static/media/slick.f97e3bbf.svg"
+  }
+]);
