da006fe983c23408fbf42f6f5190dd2f564f7a6b
Eduard Jacobs <edoter@gmail.com>
2020-02-11 21:02:49 -0800

    test

--- server.js
+++ server.js
@@ -19,23 +19,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '2mb' }));
 
 // Headers
-(function headers() {
-    app.use((req, res, next) => {
-        // Headers
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        );
+app.use((req, res, next) => {
+    // Headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
 
-        // Header options
-        if (req.method === 'OPTIONS') {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-            return res.status(200).json({});
-        };
-        next();
-    });
-})();
+    // Header options
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    };
+    next();
+});
 
 // Mongo connection
 (function mongodb() {
