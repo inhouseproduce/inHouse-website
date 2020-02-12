4241e1964df71526bbcb7a7f04a143388e05d824
Eduard Jacobs <edoter@gmail.com>
2020-02-12 14:35:57 -0800

    test

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -4,6 +4,7 @@ export const controlPi = data => {
     return async (dispatch, getState) => {
         try {
             let request = await axios.post('/control/pi/', data);
+            console.log('checking xxx--', request)
         }
         catch (error) {
             throw error;
