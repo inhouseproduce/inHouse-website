28fc833748371c0a24463e1125edd21780a0ea40
Eduard Jacobs <edoter@gmail.com>
2020-02-11 21:23:26 -0800

    test

--- server.js
+++ server.js
@@ -70,11 +70,35 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require( 'http' ).createServer( app );
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 };
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '2mb' }));
+
+// Headers
+app.use((req, res, next) => {
+    // Headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    // Header options
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    };
+    next();
+});
+
+require('./models')(app);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
