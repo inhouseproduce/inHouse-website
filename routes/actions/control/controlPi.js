43ffb2845ea13121aa6c1d3665d4921d48cee604
Eduard Jacobs <edoter@gmail.com>
2020-02-13 08:05:25 -0800

    test

--- routes/actions/control/controlPi.js
+++ routes/actions/control/controlPi.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
         let client = await db.Client.findOne({ _id: req.body.id });
 
         // Make request to client
-        let url = `http://${client.uuid}:${(process.env.PORT || 80)}/control/`;
+        let url = `http://${client.uuid}.balena-devices.com/control/`;
         let request = await axios.post(url, req.body);
         console.log('checking', req.body);
 
