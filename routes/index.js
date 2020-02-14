2a4f67324b968be82620d463a0bf2597bbb68911
Eduard Jacobs <edoter@gmail.com>
2020-02-13 21:24:01 -0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,8 @@ const routes = require('./routes.json');
 module.exports = app => {
     routes.routes.map(route => {
         let { method, url, action, to } = route;
-
-        let request = app[method];
-        let reqAction = actions[to][action];
-
-        request(url, (req, res) => {
-            reqAction(req, res);
+        app[method](url, (req, res) => {
+            actions[to][action](req, res);
         });
     });
 };
