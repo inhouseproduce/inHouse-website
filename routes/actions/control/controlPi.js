1063e930910d4c9510a3221023538f816e6e6b30
Eduard Jacobs <edoter@gmail.com>
2020-02-14 17:26:16 -0800

    auth pi to web

--- routes/actions/control/controlPi.js
+++ routes/actions/control/controlPi.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcrypt');
+
 const db = require('../../../models');
 
 module.exports = async (req, res) => {
@@ -6,9 +8,29 @@ module.exports = async (req, res) => {
         // Find Client with id
         let client = await db.Client.findOne({ _id: req.body.id });
 
+        // Extract body
+        let { status, action } = req.body;
+
         // Make request to client
-        let url = `http://${client.uuid}.balena-devices.com/control/`;
-        let request = await axios.post(url, req.body);
+        let url = `https://${client.uuid}.balena-devices.com/control/`;
+
+        // Token data
+        let tokenData = {
+            client: client.client,
+            uuid: bcrypt.hashSync(client.uuid, 10)
+        };
+
+        // Generate token
+        let token = await jwt.sign(tokenData, 'secret', {
+            algorithm: 'HS256'
+        });
+
+        // Make request
+        let request = await axios.post(url, { status, action }, {
+            headers: {
+                'authorization': `Bearer ${token}`
+            },
+        });
 
         res.status(200).json(url);
     }
