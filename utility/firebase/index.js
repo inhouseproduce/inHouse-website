d047c0b0c280960c1b58dc76aeb7fb0f54ef46fc
Eduard Jacobs <edoter@gmail.com>
2020-02-16 19:19:19 -0800

    fix

--- utility/firebase/index.js
+++ utility/firebase/index.js
@@ -5,6 +5,5 @@ module.exports.initializeApp = () => {
     admin.initializeApp({
         credential: admin.credential.cert(config),
         databaseURL: process.env.FB_DB_URL,
-        private_key: (process.env.FB_PRIVATE_KEY).replace(/\\n/g, '\n')
     });
 };
