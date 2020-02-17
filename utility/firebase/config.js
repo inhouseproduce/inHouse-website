ee66f7737948794dc201ff02feea977cfa0021c2
Eduard Jacobs <edoter@gmail.com>
2020-02-16 18:15:31 -0800

    fix

--- utility/firebase/config.js
+++ utility/firebase/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "type": "service_account",
-    "project_id": process.env.FB_PROJECT_ID,
-    "private_key_id": process.env.FB_PRIVATE_KEY_ID,
-    "private_key": process.env.FB_PRIVATE_KEY,
-    "client_email": process.env.FB_CLIENT_EMAIL,
-    "client_id": process.env.FB_CLIENT_ID,
-    "auth_uri": process.env.FB_AUTH_URI,
-    "token_uri": process.env.FB_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FB_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FB_CLIENT_X509_CERT_URL
+    type: 'service_account',
+    project_id: process.env.FB_PROJECT_ID,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY,
+    client_email: process.env.FB_CLIENT_EMAIL,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FB_CLIENT_X509_CERT_URL
 };
 
